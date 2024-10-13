e6b99091499f2fe0f6991b2a395f2893e98963d6
joe <kirillpavlov4214@gmail.com>
2024-10-13 05:31:52 +0300

    fixes...

--- utils/unpack_models.sh
+++ utils/unpack_models.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-unzip ./models/bert_extra.zip ./bert_extra
-unzip ./models/vosk-model-small-ru-0.22.zip ./vosk-model-small-ru-0.22/
-unzip ./models/bert_noised.zip ./bert_noised
+echo "создание директорий..."
+mkdir -p ./bert_extra
+mkdir -p ./vosk-model-small-ru-0.22/
+mkdir -p ./bert_noised
+
+echo "разархивирование..."
+unzip ./models/bert_extra.zip -d ./bert_extra
+unzip ./models/vosk-model-small-ru-0.22.zip -d ./vosk-model-small-ru-0.22/
+unzip ./models/bert_noised.zip -d ./bert_noised
 
 echo "Модели успешно распакованы!"
